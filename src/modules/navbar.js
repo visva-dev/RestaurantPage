2256aa7d7194f86d943fad0ac100bfafbc697181
Visvaldas <visva.rapalis@gmail.com>
2020-11-18 12:57:41 +0200

    Create navbar with JavaScript

--- src/modules/navbar.js
+++ src/modules/navbar.js
@@ -1,5 +1,27 @@
-function nav() {
+const nav = () => {
   console.log('This is a Restaurant navbar!');
+  const nav = document.createElement('nav');
+  const unorderedList = document.createElement('ul');
+  const list = document.createElement('li');
+  const anchor = document.createElement('a');
+  const secondList = document.createElement('li');
+  const secondAnchor = document.createElement('a');
+  const thirdList = document.createElement('li');
+  const thirdAnchor = document.createElement('a');
+  const content = document.getElementById('content');
+  anchor.textContent = 'Home';
+  secondAnchor.textContent = 'Menu';
+  thirdAnchor.textContent = 'Contact';
+  list.appendChild(anchor);
+  secondList.appendChild(secondAnchor);
+  thirdList.appendChild(thirdAnchor);
+  unorderedList.appendChild(list);
+  unorderedList.appendChild(secondList);
+  unorderedList.appendChild(thirdList);
+  nav.appendChild(unorderedList);
+
+  content.appendChild(nav);
+  nav.setAttribute('class', 'flex-nav');
 }
 
 export default nav;
